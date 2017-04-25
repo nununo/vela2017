5e2b358d554dc31135b66fb51d4fc1e8dc773f60
Nuno Godinho <mail@nunogodinho.com>
2017-04-25 20:20:08 +0100

    OMX Player is finished one more attempt

--- src/MovieOMXPlayer.cpp
+++ src/MovieOMXPlayer.cpp
@@ -32,7 +32,7 @@ MovieOMXPlayer::MovieOMXPlayer(string _filename, bool _loop) {
 
 //--------------------------------------------------------------
 void MovieOMXPlayer::update() {
-  if (!loop && !isPaused() && getPosition()>0.8f && getPosition()==lastPosition) {
+  if (!loop && !isPaused() && getPosition()>0.8f && (getPosition()==lastPosition || getPosition()>1)) {
     finished = true;
   }
   lastPosition = getPosition();
