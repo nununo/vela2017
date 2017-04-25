357d15944d7c47c8147dd90822602a0d5d8fd462
Nuno Godinho <mail@nunogodinho.com>
2017-04-25 13:04:36 +0100

    alpha now being set during update

--- src/MovieOfVideoPlayer.h
+++ src/MovieOfVideoPlayer.h
@@ -20,7 +20,7 @@ public:
   virtual float getDuration() {return movie->getDuration();}
   virtual void rewind() {movie->setPosition(0); movie->play();}
   virtual void update() {movie->update();}
-  virtual void draw(float x, float y, float width, float height, int alpha);
+  virtual void draw(float x, float y, float width, float height);
   virtual void setPaused(bool pause=true) {movie->setPaused(pause);}
   virtual bool isPaused() {return movie->isPaused();}
 
