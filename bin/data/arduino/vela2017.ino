6462aed061b50c57863beb84e529e4c44d9977e7
Nuno Godinho <mail@nunogodinho.com>
2017-05-12 23:12:05 +0100

    Arduino file added to data folder

---
+++ bin/data/arduino/vela2017.ino
@@ -0,0 +1,66 @@
+int lastValue;
+int count;
+
+//--------------------------------------------
+void setup(){
+  Serial.begin(9600);  
+  lastValue = 0;
+  count = 0;
+  analogReference(INTERNAL);
+}
+
+//--------------------------------------------
+void loop(){
+  int value;
+  
+  value = runningAverage(analogRead(A0));
+
+  if (count > 100) {
+    sendValue(value);
+    count = 0;
+      //Serial.flush();
+  }
+
+  //wait a little bit so we don't send too much data
+  delay(1);
+
+  count++;
+}
+
+//--------------------------------------------
+long runningAverage(int M) {
+  #define LM_SIZE 16
+  static int LM[LM_SIZE];
+  static byte index = 0;
+  static long sum = 0;
+  static byte count = 0;
+
+  sum -= LM[index];
+  LM[index] = M;
+  sum += LM[index];
+  index++;
+  index = index % LM_SIZE;
+  if (count < LM_SIZE) count++;
+
+  return sum / count;
+}
+
+//--------------------------------------------
+void sendValue(int value) {
+  
+  //we split the 0 - 1023 number into two bytes so we are sending a message of a known length
+  unsigned char byte1;
+  unsigned char byte2;
+  
+  //gnarly code to turn the int into two bytes
+  byte1 = value &0xFF;
+  byte2 = (value >> 8) &0xFF;
+        
+  //send both bytes
+  //Serial.write(byte1);
+  //Serial.write(byte2);
+
+  Serial.print(' ');
+  Serial.write(byte1);
+  Serial.write(byte2);
+}
