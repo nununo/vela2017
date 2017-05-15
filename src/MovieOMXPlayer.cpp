0ed57a4057eff7a660883656ec55da215fb7e6d2
Nuno Godinho <mail@nunogodinho.com>
2017-05-15 02:50:18 +0100

    virtual keyword in cpp removed

--- src/MovieOMXPlayer.cpp
+++ src/MovieOMXPlayer.cpp
@@ -30,7 +30,7 @@ MovieOMXPlayer::MovieOMXPlayer(string _filename, bool _loop) {
 }
 
 //--------------------------------------------------------------
-virtual void setup() {
+void setup() {
   if (!movie.isTextureEnabled()) {
     ofxOMXPlayerSettings settings = movie.getSettings();
     settings.enableTexture = true;
