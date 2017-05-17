eca6c7ef91c493afe2ea6895b25187aa9e5a9c73
Nuno Godinho <mail@nunogodinho.com>
2017-05-17 03:46:19 +0100

    Experiment showed that the new clip is not set to invisible So the mystery remains

--- src/ClipLayer.cpp
+++ src/ClipLayer.cpp
@@ -21,10 +21,8 @@ ClipLayer::~ClipLayer() {
 
 //-----------------------------------------------------------------------
 void ClipLayer::update() {
-  if (!clip->isPlaying()) {
+  if (!clip->isPlaying())
     setVisible(false);
-    ofLogNotice() << clip->getFilename() << " not playing, set invisible";
-  }
   
   if (isVisible())
     clip->update();
